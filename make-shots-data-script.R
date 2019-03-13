01d71f621091c0ed7734d364e57b3c8745a4933c
Jack Tseng <jekatk1209@berkeley.edu>
2019-03-12 23:13:20 -0700

    some work done sleep now

--- make-shots-data-script.R
+++ make-shots-data-script.R
@@ -1,4 +1,24 @@
 #Title: make-shots-data-script
-#Description: This script is a script for make shots.
-#Inputs:
-#Outputs: 
+#Description: This script is a script for manipulating the shot data of the starting five of the GSW
+#Inputs: five CSV files, one for each starter (Curry, Durant, Iguodala, Green, Thompson)
+#Outputs: ########do this#########
+
+#imports
+library(dplyr)
+library(ggplot2)
+
+#read in files
+iguodala<-read.csv("../data/andre-iguodala.csv",colClasses=c("character","character","integer","integer","integer","integer","character","character","character","integer","character","real","real"),stringsAsFactors = FALSE)
+green<-read.csv("../data/draymond-green.csv",colClasses=c("character","character","integer","integer","integer","integer","character","character","character","integer","character","real","real"),stringsAsFactors = FALSE)
+durant<-read.csv("../data/kevin-durant.csv",colClasses=c("character","character","integer","integer","integer","integer","character","character","character","integer","character","real","real"),stringsAsFactors = FALSE)
+thompson<-read.csv("../data/klay-thompson.csv",colClasses=c("character","character","integer","integer","integer","integer","character","character","character","integer","character","real","real"),stringsAsFactors = FALSE)
+curry<-read.csv("../data/stephen-curry.csv",colClasses=c("character","character","integer","integer","integer","integer","character","character","character","integer","character","real","real"),stringsAsFactors = FALSE)
+
+#add name column
+mutate(iguodala, name = "Andre Iguodala")
+mutate(green, name = "Draymond Green")
+mutate(durant, name = "Kevin Durant")
+mutate(thompson, name = "Klay Thompson")
+mutate(curry, name = "Stephen Curry")
+
+
